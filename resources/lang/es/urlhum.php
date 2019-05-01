b5ecf6890ea9f5c79152c49ce37e6da4d5a78219
Martín Peveri <martinpeveri@gmail.com>
2019-05-01 19:00:51 -0300

    Spanish language is added to the translations

---
+++ resources/lang/es/urlhum.php
@@ -0,0 +1,24 @@
+<?php
+
+return array (
+  'privacypolicy' => 'Políticas de privacidad',
+  'and' => 'y',
+  'termsofuse' => 'Términos de uso',
+  'dashboard' => 'Dashboard',
+  'design_by' => 'Diseñado por',
+  'users' => 'Usuarios',
+  'welcome' => '¡Bienvenido!',
+  'accessFeatures' => 'Regístrese o inicie sesión para acceder a todas las funciones.',
+  'search' => 'Buscar',
+  'account' => 'Cuenta',
+  'logout' => 'Cerrar sesión',
+  'register' => 'Registro',
+  'login' => 'Iniciar sesión',
+  'home' => 'Home',
+  'save' => 'Grabar',
+  'error' => 'Error',
+  'send' => 'Enviar',
+  'close' => 'Cerrar',
+  'edit' => 'Editar',
+  'user' => 'Usuario',
+);
