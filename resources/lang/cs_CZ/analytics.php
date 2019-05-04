9f7e14e1c7d608671669aa95e55a4ee15a25ac6d
Christian la Forgia <christianlaforgia97@gmail.com>
2019-05-04 17:35:11 +0200

    New translations analytics.php (Czech)

--- resources/lang/cs_CZ/analytics.php
+++ resources/lang/cs_CZ/analytics.php
@@ -7,27 +7,27 @@ return array (
     'referer' => 'Referer',
     'list' => 
     array (
-      'results' => ':firstItem / :lastItem 共 :num',
+      'results' => 'Showing :firstItem - :lastItem referers of :num total',
     ),
-    'na' => '仍无引荐网址，请分享这个链接！',
-    'best' => '最佳来源',
+    'na' => 'Still no referers to report. Share the URL to increase referrers!',
+    'best' => 'Best referers',
   ),
   'click' => 
   array (
-    'click' => '点击量',
-    'reals' => '真实点击量',
-    'clicks' => '点击量',
-    'today' => '今日点击量',
-    'real' => '真实点击量',
+    'click' => 'Click',
+    'reals' => 'Real Clicks',
+    'clicks' => 'Clicks',
+    'today' => 'Clicks today',
+    'real' => 'Real Click',
   ),
   'show' => 
   array (
-    'title' => '短链接分析',
+    'title' => 'Analytics for Short URL',
   ),
   'country' => 
   array (
-    'real' => '真实浏览量（按国家/地区排序）',
-    'na' => '抱歉，我们没有足够的数据来显示国家/地区的访问量，分享这个网址！',
+    'real' => 'Real Views By Country',
+    'na' => 'Sorry, we don\'t have enough data to display countries visits. Share this URL!',
     'views' => '按国家/地区显示',
   ),
   'view' => 
