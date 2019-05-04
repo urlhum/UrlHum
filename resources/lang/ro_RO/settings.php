701db7a519d5c1f72866e944f09d06712254edcf
Christian la Forgia <christianlaforgia97@gmail.com>
2019-05-04 12:25:45 +0200

    New translations settings.php (Romanian)

--- resources/lang/ro_RO/settings.php
+++ resources/lang/ro_RO/settings.php
@@ -1,18 +1,32 @@
 <?php
 
-return [
-  /*
-  |--------------------------------------------------------------------------
-  | Settings Language Lines
-  |--------------------------------------------------------------------------
-  |
-  | The following language lines are used for Laravel Backpack - Settings
-  |
-  */
-  'name'             => '名称',
-  'value'            => '值',
-  'description'      => '描述',
-  'setting_singular' => '设置',
-  'setting_plural'   => '设置',
-
-];
+return array (
+  'settings' => '设置',
+  'system' => '系统设置',
+  'anon_hash_alert' => '注意：如果您对IP进行匿名处理而不进行哈希处理，则分析将无法统计实际点击次数，因为无法检测用户IP。',
+  'general' => '概览',
+  'website_name' => '网站名称',
+  'website_image' => '网站图片',
+  'private' => '使网站私密',
+  'private_help' => '网站首页将重定向到登录页',
+  'registration' => '启用用户注册',
+  'public' => '在访客首页中显示最近公开创建的URL',
+  'public_help' => '在网站主页中显示有最新URLS的小挂件',
+  'privacy_policy' => '启用隐私政策页面',
+  'privacy_policy_edit' => '编辑隐私政策',
+  'tos' => '启用使用条款页面',
+  'tos_edit' => '编辑使用条款',
+  'hash' => '在数据库中Hash(哈希) IP地址',
+  'hash_anon_help' => '如果您来自欧盟国家，建议遵守GDPR',
+  'anonymize_ip' => '获取IP的国家/地区时匿名化IP地址',
+  'anonymous' => '允许匿名创建短网址',
+  'anonymous_help' => '允许未登录的用户创建短链接',
+  'deleted' => '删除的URL能否被再次创建？',
+  'reserved' => '保留的短网址（每行一个）',
+  'reserved_help' => '用户将无法创建这些短链接',
+  'reserved_restore' => '恢复默认',
+  'privacy_policy_desc' => '在此处编辑您的隐私政策，可以使用HTML语言',
+  'tos_desc' => '在此处编辑您的使用条款，可以使用HTML语言',
+  'save_changes' => '保存更改',
+  'success' => '设置已更新！',
+);
