ae34e560910d3c362c6a19c76febb8aa0ccf8e6b
Christian la Forgia <christianlaforgia97@gmail.com>
2019-05-04 13:06:53 +0200

    New translations passwords.php (Turkish)

--- resources/lang/tr_TR/passwords.php
+++ resources/lang/tr_TR/passwords.php
@@ -1,9 +1,9 @@
 <?php
 
 return array (
-  'password' => 'La password deve essere di almeno sei caratteri e deve combaciare con la conferma.',
-  'reset' => 'La tua password è stata ripristinata!',
-  'sent' => 'Ti abbiamo inviato via mail un link per ripristinare la password!',
-  'token' => 'Questo token per il reset della password non è valido.',
-  'user' => 'Non riusciamo a trovare un utente con questo indirizzo email.',
+  'password' => '密码必须至少为六个字符',
+  'reset' => '你的密码已被重置',
+  'sent' => '密码重置链接已发送到你的邮箱中！',
+  'token' => '密码重置Token无效',
+  'user' => '该用户不存在',
 );
