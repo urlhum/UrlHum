2e88b4dfcb405d2ba64122fdd7a0e0a8eaee77ba
Christian la Forgia <christianlaforgia97@gmail.com>
2019-05-04 12:45:23 +0200

    New translations settings.php (Turkish)

--- resources/lang/tr_TR/settings.php
+++ resources/lang/tr_TR/settings.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Settings Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used for Laravel Backpack - Settings
-    |
-    */
-    'name'             => 'Naam',
-    'value'            => 'Waarde',
-    'description'      => 'Beschrijving',
-    'setting_singular' => 'instelling',
-    'setting_plural'   => 'instellingen',
+  /*
+  |--------------------------------------------------------------------------
+  | Settings Language Lines
+  |--------------------------------------------------------------------------
+  |
+  | The following language lines are used for Laravel Backpack - Settings
+  |
+  */
+  'name'             => 'ຊື່',
+  'value'            => 'ຄ່າທີ່ລະບຸ',
+  'description'      => 'ຄໍາອະທິບາຍ',
+  'setting_singular' => 'ການຕັ້ງຄ່າ',
+  'setting_plural'   => 'ການຕັ້ງຄ່າຕ່າງໆ',
 
 ];
