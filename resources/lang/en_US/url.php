55aa0b2732637b739ca759a016f2857746013c49
Christian la Forgia <christianlaforgia97@gmail.com>
2019-05-04 11:43:16 +0200

    New translations url.php (English)

---
+++ resources/lang/en_US/url.php
@@ -0,0 +1,49 @@
+<?php
+
+return array (
+  'list' => 'URLs list',
+  'edit' => 
+  array (
+    'edit' => 'Edit URL',
+    'short' => 'Edit Short URL',
+  ),
+  'created' => 'URL created at :date',
+  'url' => 'URL',
+  'my' => 
+  array (
+    'my' => 'My Short URLs',
+    'list' => 'Showing :firstItem - :lastItem Short URLs of :num total',
+  ),
+  'public' => 'Latest public URLs',
+  'short' => 'Short URL',
+  'new' => 'New URL',
+  'long' => 'Long URL',
+  'delete' => 
+  array (
+    'confirm' => 'Are you sure you want to delete this Short URL? All related Analytics data will be lost.',
+    'delete' => 'Delete URL',
+  ),
+  'listres' => 'Showing :firstItem - :lastItem Short URLs of :num total',
+  'updated' => 'URL updated at :date',
+  'destination' => 'Destination',
+  'by' => 'Created By',
+  'stats' => 'View stats',
+  'options' => 
+  array (
+    'hide' => 'Hide from Public URLs page',
+    'private_stats' => 'Make this URL statistics private',
+    'destination' => 'Destination URL',
+    'options' => 'Options',
+    'custom' => 'Custom Short URL',
+  ),
+  'id' => 'ID',
+  'createdStr' => 'Created',
+  'action' => 'Action',
+  'existing_long' => 'This Long URL already exists',
+  'success' => 'Success! Short URL created',
+  'existing_custom' => 'The short URL :url already exists. Please choose another Short URL',
+  'new_long_text' => 'Create new Short URL',
+  'latest' => 'Latest URLs',
+  'all' => 'See all',
+  'urls' => 'URLs',
+);
