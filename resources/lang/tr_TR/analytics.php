f8a31e2bb1164326e77b61b0c1d614d4ec6446ca
Christian la Forgia <christianlaforgia97@gmail.com>
2019-05-04 15:54:12 +0200

    New translations analytics.php (Turkish)

--- resources/lang/tr_TR/analytics.php
+++ resources/lang/tr_TR/analytics.php
@@ -1,45 +1,45 @@
 <?php
 
 return array (
-  'analytics' => 'Analytics',
-  'click' => 
-  array (
-    'click' => 'Click',
-    'clicks' => 'Click',
-    'real' => 'Click reali',
-    'reals' => 'Click reali',
-    'today' => 'Click oggi',
-  ),
-  'country' => 
-  array (
-    'na' => 'Spiacenti, non abbiamo abbastanza dati per mostrare le visite per paese. Condividi questo URL!',
-    'real' => 'Visualizzazioni reali per Paese',
-    'views' => 'Visualizzazioni per Paese',
-  ),
   'referer' => 
   array (
-    'best' => 'Migliori referers',
+    'referers' => 'Referers',
+    'referer' => 'Referer',
     'list' => 
     array (
       'results' => 'Mostrando :firstItem - :lastItem referers di :num totali',
     ),
     'na' => 'Ancora nessun referer da mostrare. Condividi questo URL per aumentarli!',
-    'referer' => 'Referer',
-    'referers' => 'Referers',
+    'best' => 'Migliori referers',
+  ),
+  'click' => 
+  array (
+    'click' => 'Click',
+    'reals' => 'Click reali',
+    'clicks' => 'Click',
+    'today' => 'Click oggi',
+    'real' => 'Click reali',
   ),
   'show' => 
   array (
     'title' => 'Analytics per Short URL',
   ),
+  'country' => 
+  array (
+    'real' => 'Visualizzazioni reali per Paese',
+    'na' => 'Spiacenti, non abbiamo abbastanza dati per mostrare le visite per paese. Condividi questo URL!',
+    'views' => 'Visualizzazioni per Paese',
+  ),
+  'view' => 
+  array (
+    'reals' => 'Visualizzazioni Reali',
+    'views' => 'Visualizzazioni',
+    'view' => 'Visualizzazione',
+    'real' => 'Visualizzazione Reale',
+  ),
   'url' => 
   array (
     'created' => 'Short URL creati',
   ),
-  'view' => 
-  array (
-    'real' => 'Visualizzazione Reale',
-    'reals' => 'Visualizzazioni Reali',
-    'view' => 'Visualizzazione',
-    'views' => 'Visualizzazioni',
-  ),
+  'analytics' => 'Analytics',
 );
