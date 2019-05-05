cdce40c8629c91b7b8b5851363cdd67228cca486
ahmed <ahmed_aboud_ali@yahoo.com>
2019-05-05 12:59:19 +0200

    Update AnalyticsController.php

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -56,7 +56,7 @@ class AnalyticsController extends Controller
             },
         ])->where('short_url', $url)->firstOrFail();
 
-        if ($urlWithRelationsUrl->hide_stats && !$this->url->OwnerOrAdmin($url)) {
+        if ($urlWithRelationsUrl->hide_stats == '1' && !$this->url->OwnerOrAdmin($url)) {
             abort(403);
         }
 
