c94e6b6aaeacc384e2d5d37e31328107359cdf30
Christian la Forgia <christianlaforgia97@gmail.com>
2019-05-15 12:17:43 +0200

    New translations analytics.php (Persian)

---
+++ resources/lang/fa_IR/analytics.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * UrlHum (https://urlhum.com)
+ *
+ * @link      https://github.com/urlhum/UrlHum
+ * @copyright Copyright (c) 2019 Christian la Forgia
+ * @license   https://github.com/urlhum/UrlHum/blob/master/LICENSE.md (MIT License)
+ */
+
+return [
+  'referer' => [
+    'referers' => 'Referers',
+    'referer' => 'Referer',
+    'list' => [
+      'results' => 'Showing :firstItem - :lastItem referers of :num total',
+    ],
+    'na' => 'Still no referers to report. Share the URL to increase referrers!',
+    'best' => 'Best referers',
+  ],
+  'click' => [
+    'click' => 'Click',
+    'reals' => 'Real Clicks',
+    'clicks' => 'Clicks',
+    'today' => 'Clicks today',
+    'real' => 'Real Click',
+  ],
+  'show' => [
+    'title' => 'Analytics for Short URL',
+  ],
+  'country' => [
+    'real' => 'Real Views By Country',
+    'na' => 'Sorry, we don\'t have enough data to display countries visits. Share this URL!',
+    'views' => 'Views By Country',
+  ],
+  'view' => [
+    'reals' => 'Real Views',
+    'views' => 'Views',
+    'view' => 'View',
+    'real' => 'Real View',
+  ],
+  'url' => [
+    'created' => 'Short URLs created',
+  ],
+  'analytics' => 'Analytics',
+];
