54a78f1f77d5c41590837201d38afed7413e8388
Christian la Forgia <christianlaforgia97@gmail.com>
2019-05-15 12:17:45 +0200

    New translations pagination.php (Persian)

---
+++ resources/lang/fa_IR/pagination.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * UrlHum (https://urlhum.com)
+ *
+ * @link      https://github.com/urlhum/UrlHum
+ * @copyright Copyright (c) 2019 Christian la Forgia
+ * @license   https://github.com/urlhum/UrlHum/blob/master/LICENSE.md (MIT License)
+ */
+
+return [
+  'previous' => '&laquo;',
+  'next' => '&raquo;',
+];
