c62f8a8f34148b6bf7bedd07276a6cc43c7b72bd
Christian la Forgia <christianlaforgia97@gmail.com>
2019-05-21 19:02:00 +0200

    New translations analytics.php (Italian)

--- resources/lang/it_IT/analytics.php
+++ resources/lang/it_IT/analytics.php
@@ -25,6 +25,9 @@ return [
     'clicks' => 'Click',
     'today' => 'Click oggi',
     'real' => 'Click reali',
+    'latest' => 'Latest Click',
+    'latests' => 'Latest Clicks',
+    'na' => 'Sorry, we don\'t have enough data to display latest clicks.'
   ],
   'show' => [
     'title' => 'Analytics per Short URL',
@@ -44,4 +47,5 @@ return [
     'created' => 'Short URL creati',
   ],
   'analytics' => 'Analytics',
+  'time' => 'Time'
 ];
