f034e06f17bd5b9caa1621c3a56f3ce455a5465c
Christian la Forgia <christianlaforgia97@gmail.com>
2019-05-21 19:02:14 +0200

    New translations url.php (Chinese Simplified)

--- resources/lang/zh_CN/url.php
+++ resources/lang/zh_CN/url.php
@@ -17,7 +17,7 @@ return [
   'created' => '在 :date 创建的URL',
   'url' => 'URL',
   'my' => [
-    'my' => '我的短链接',
+    'my' => '我的短连接',
     'list' => ':firstItem / :lastItem 共 :num',
   ],
   'public' => '最新公开链接',
@@ -25,7 +25,7 @@ return [
   'new' => '新链接',
   'long' => '长链接',
   'delete' => [
-    'confirm' => '您确定要删除此短链接吗？ 所有相关的分析数据都将消失！',
+    'confirm' => '您确定要删除此短网址吗？ 所有相关的分析数据都将消失！',
     'delete' => '删除',
   ],
   'listres' => ':firstItem / :lastItem 共 :num',
@@ -34,8 +34,8 @@ return [
   'by' => '创建者',
   'stats' => '查看统计信息',
   'options' => [
-    'hide' => '将此 URL 从公开页面隐藏',
-    'private_stats' => '将此 URL 统计信息设为私有',
+    'hide' => '将此URL从公开页面隐藏',
+    'private_stats' => '将此URL统计信息设为私有',
     'destination' => 'URL 地址',
     'options' => '选项',
     'custom' => '自定义短链接',
@@ -45,9 +45,9 @@ return [
   'action' => '操作',
   'existing_long' => '此长链接已存在！',
   'success' => '恭喜，短链接创建成功！',
-  'existing_custom' => '短链接 :url 已存在，请尝试更换',
+  'existing_custom' => '短链接 :url already 已存在，请尝试更换',
   'new_long_text' => '创建新的短链接',
-  'latest' => '最新 URL',
+  'latest' => '最新URL',
   'all' => '查看所有',
   'urls' => 'URL',
 ];
