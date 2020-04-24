a3ad13a22eedfd64a1216ba129471c7a83210a74
Christian la Forgia <christianlaforgia97@gmail.com>
2020-04-24 22:45:21 +0200

    New translations passwords.php (Turkish)

--- resources/lang/tr_TR/passwords.php
+++ resources/lang/tr_TR/passwords.php
@@ -9,9 +9,9 @@
  */
 
 return [
-  'password' => 'Şifre en az 6 karakter olmalı ve eşleşmelidir.',
-  'reset' => 'Şifreniz sıfırlandı!',
-  'sent' => 'Şifre sıfırlama bağlantınızı e-posta ile gönderdik!',
-  'token' => 'Şifre sıfırlama kodu geçersiz.',
-  'user' => 'Bu e-posta adresine sahip bir kullanıcı yok.',
+  'password' => 'Passwords must be at least six characters and match the confirmation.',
+  'reset' => 'Your password has been reset!',
+  'sent' => 'We have e-mailed your password reset link!',
+  'token' => 'This password reset token is invalid.',
+  'user' => 'We can\'t find a user with that e-mail address.',
 ];
